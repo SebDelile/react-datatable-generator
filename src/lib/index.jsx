caa56747fb27cb7cd584eb34dd214e7f90240fd9
Delile Sebastien <sebastiendelile@gmail.com>
2021-10-26 17:23:21 +0200

    feat: add filter handler feature
    implement the filtering feature
    add unit test to the handler
    add the filterhandler to index.js

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Filter } from './components/Filter/Filter.jsx';
 import { Table } from './components/Table/Table.jsx';
+import { filterHandler } from './handlers/filterHandler/filterHandler.js';
 
 const Datatable = ({ headings, data }) => {
   const [filterKeyword, setFilterKeyword] = useState('');
+  const [dataToDisplay, setDataToDisplay] = useState(data);
+
+  useEffect(() => {
+    const filteredData = filterHandler(data, filterKeyword);
+    setDataToDisplay(filteredData);
+  }, [data, filterKeyword]);
+
   return (
     <article>
       <Filter setFilterKeyword={setFilterKeyword} />
-      <Table headings={headings} data={data} />
+      <Table headings={headings} data={dataToDisplay} />
     </article>
   );
 };
