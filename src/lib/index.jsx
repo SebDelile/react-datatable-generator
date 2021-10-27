dcbcaf187dfa40c83a7af3e109fc09fea90ce2ae
Delile Sebastien <sebastiendelile@gmail.com>
2021-10-27 19:10:16 +0200

    feat: add sorthandler feature
    add sorthandler feature
    sort is implemented in index.jsx
    add corresponding tests

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -3,16 +3,31 @@ import { Filter } from './components/Filter/Filter.jsx';
 import { TableHeading } from './components/TableHeading/TableHeading.jsx';
 import { TableBody } from './components/TableBody/TableBody.jsx';
 import { filterHandler } from './handlers/filterHandler/filterHandler.js';
+import { sortHandler } from './handlers/sortHandler/sortHandler.js';
 
 const Datatable = ({ headings, data }) => {
   const [filterKeyword, setFilterKeyword] = useState('');
-  const [currentSort, setCurrentSort] = useState({ key: null, direction: 1 });
-  const [dataToDisplay, setDataToDisplay] = useState(data);
+  const [currentSort, setCurrentSort] = useState({
+    key: null,
+    direction: 1,
+    type: 'string',
+  });
+
+  const [filteredData, setFilteredData] = useState(data);
+  const [sortedData, setSortedData] = useState(filteredData);
+  const [displayedData, setDisplayedData] = useState(sortedData);
 
   useEffect(() => {
-    const filteredData = filterHandler(data, filterKeyword);
-    setDataToDisplay(filteredData);
+    setFilteredData(filterHandler(data, filterKeyword));
   }, [data, filterKeyword]);
+
+  useEffect(() => {
+    setSortedData(sortHandler(filteredData, currentSort));
+  }, [filteredData, currentSort]);
+
+  useEffect(() => {
+    setDisplayedData(sortedData);
+  }, [sortedData, currentSort]);
 
   return (
     <article>
@@ -26,7 +41,7 @@ const Datatable = ({ headings, data }) => {
           currentSort={currentSort}
           setCurrentSort={setCurrentSort}
         />
-        <TableBody headings={headings} data={dataToDisplay} />
+        <TableBody headings={headings} data={displayedData} />
       </table>
     </article>
   );
