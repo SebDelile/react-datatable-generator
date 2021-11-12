2961a30a31a4de9c804a7bee5417f73f30ff1df4
Delile Sebastien <sebastiendelile@gmail.com>
2021-11-12 13:20:13 +0100

    fix: columnsMinWidthCalc
    function now works well on firefox
    font property not recognized in FF, fontStyle, fontFamily yes
    use a Math.ceil to avoid decimal approximation issues

--- src/lib/utils/columnsMinWidthCalc/columnsMinWidthCalc.js
+++ src/lib/utils/columnsMinWidthCalc/columnsMinWidthCalc.js
@@ -1,7 +1,8 @@
 export const columnsMinWidthCalc = (headings, data) => {
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
-  context.font = getComputedStyle(document.documentElement).font;
+  const rootStyle = getComputedStyle(document.documentElement);
+  context.font = `${rootStyle.fontSize} ${rootStyle.fontFamily}`;
   const bodyColumnMinWidth = headings.map((heading) =>
     data
       .map((item) => item[heading.key])
@@ -9,9 +10,11 @@ export const columnsMinWidthCalc = (headings, data) => {
   );
   context.font = 'bold ' + context.font;
   const columnMinWidth = headings.map((heading, index) =>
-    Math.max(
-      bodyColumnMinWidth[index],
-      context.measureText(heading.label).width + 16 //get sort image width from options
+    Math.ceil(
+      Math.max(
+        bodyColumnMinWidth[index],
+        context.measureText(heading.label).width + 16 //get sort image width from options
+      )
     )
   );
   canvas.remove();
